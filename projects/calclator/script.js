e9d29cd7d0f51518ecd40db515b52406311b2240
Prosper2237 <adogbeji.prosper@gmail.com>
2025-09-02 03:26:12 -0700

    updated my calculator folder

--- projects/calclator/script.js
+++ projects/calclator/script.js
@@ -1,17 +1,16 @@
-function appendValue(value) {
-    document.getElementById("display").value += value;
+function press(value) {
+document.getElementById("display").value += value;
 }
 
 function calculate() {
-    try {
-        document.getElementById("display").value =
-            eval(document.getElementById("display").value);
-    } catch {
-        alert ("Invalid Expression");
-    }
+   try {
+     document.getElementById("display").value = eval(document.getElementById("display").value);
 
-}
+   } catch {
+    alert("Invalid Expression");
+   }
+} 
 
 function clearDisplay() {
-     document.getElementById("display").value = "";
+    document.getElementById("display").value = "";
 }
